d40d767685bcf091a1ca958f9585b0c1be880406
Maxwell Karp <maxwell.karp@yahoo.com>
2015-04-29 11:00:11 -0700

    Two Week Assessment

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.dan').click(function() {
+        alert('Hey you clicked on me');
+    });
+     $('#iwao').click(function() {
+       $(this).fadeOut('slow');
+    });
+});
+
